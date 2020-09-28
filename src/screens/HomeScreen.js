dd907af888485d3424f10f173d6300b6378e4f14
SharminMou <sharminnaj@iut-dhaka.edu>
2020-09-29 02:40:24 +0600

    home page done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,82 @@
 import React from "react";
-import {Text, StyleSheet, Button, View} from "react-native";
+import {Text, StyleSheet, Button, View, Image, TouchableOpacity} from "react-native";
 
 const HomeScreen = (props)=>{
     console.log(props);
     return(
-      <View>
-         <Text style={styles.textStyle}>HomeScreen</Text>
-         <Button
-            title="Go To List Screen"
-             onPress={function () {
-              props.navigation.navigate("List");
-            }}
-         />
-      </View>
+      <View style={styles.rootViewStyle}>
+         {/* <Text style={styles.textStyle}>Home</Text> */}
+         <View style={{marginVertical: 20}}>
+            <Image source={require('../../assets/Islamic_University_of_Technology_logo.png')} style={styles.logoStyle}/>
+         </View>
+         <View style={{marginVertical: 18,alignItems:"center"}}>
+          <Text style={styles.textStyle}>Department of CSE</Text>
+          <Text style={styles.textStyle}>Program: SWE</Text>
+         </View>
+         
+          <View style={styles.opacityViewStyle}>
+            <TouchableOpacity 
+              style={{alignSelf: "center",}}
+              onPress={function () {
+                  props.navigation.navigate("List");
+                }}
+              >
+                <Text style={styles.opacityTextStyle}>My Profile</Text>
+              </TouchableOpacity>
+            </View>
+          
+          <View style={styles.buttonViewStyle}>
+              <Button color='#008B8B'
+                title="Semester Wise Course List"
+                onPress={function () {
+                props.navigation.navigate("List");
+                }}
+              />
+          </View>
+          <View style={styles.buttonViewStyle}>
+              <Button color='#008B8B'
+                  title="List of Faculty Members"
+                  onPress={function () {
+                  props.navigation.navigate("List");
+                  }}
+              />
+          </View>
+
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     textStyle:{
-        fontSize: 30,
-        color: 'blue',
+        fontSize: 20,
+        color: 'black',
+        fontWeight: "bold",
+    },
+    rootViewStyle:{
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    logoStyle:{
+      height: 300,
+      width: 300,
+      resizeMode:"contain",
+    },
+    opacityTextStyle:{
+      fontSize: 15,
+      color: "white",
+      fontWeight: "bold",
+    },
+    opacityViewStyle:{
+      backgroundColor:'#008B8B',
+      marginTop: 10,
+      padding: 10,
+      width: 250,
+
+    },
+    buttonViewStyle:{
+      marginTop: 10,
+      width: 250,
+
     },
 });
 
